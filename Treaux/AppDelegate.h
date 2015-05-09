c8386d62e836e4ab9f29792f2172a9ecc2bb2aed
Johann Kerr <johannkerr@gmail.com>
2015-05-09 18:09:54 -0400

    Initial Commit

---
+++ Treaux/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Treaux
+//
+//  Created by Johann Kerr on 5/9/15.
+//  Copyright (c) 2015 Johann Kerr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Treaux/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Treaux
+//
+//  Created by Johann Kerr on 5/9/15.
+//  Copyright (c) 2015 Johann Kerr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

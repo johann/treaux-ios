88dfbab6ab48554498867c38b812634c869510f6
Johann Kerr <johannkerr@gmail.com>
2015-05-11 17:23:03 -0400

    first commit

---
+++ Treaux/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  Treaux
+//
+//  Created by Johann Kerr on 5/11/15.
+//  Copyright (c) 2015 Johann Kerr. All rights reserved.
+//
+
+#import "MTLModel.h"
+
+@interface User : MTLModel
+
+@end
---
+++ Treaux/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Treaux
+//
+//  Created by Johann Kerr on 5/11/15.
+//  Copyright (c) 2015 Johann Kerr. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
